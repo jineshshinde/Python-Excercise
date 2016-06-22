8365122aa7c7cfd640b5ca3a3ff253b8bb462f02
jineshshinde <samratjinesh619@gmail.com>
2016-06-22 18:25:20 +0530

    Create List_ends.py

---
+++ List_ends.py
@@ -0,0 +1,10 @@
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and 
+last elements of the given list.
+
+import random
+
+a = [random.randint(1,100) for x in range(10)]
+slice(a)
+
+def slice(arr):
+    return arr[:1] + arr[-1:] if len(arr) > 1 else arr
