8c59e69cd1dca91e0647c3bff431c3c9f4d62dfe
jineshshinde <samratjinesh619@gmail.com>
2016-06-22 18:30:05 +0530

    Create Fibonnaci.py

---
+++ Fibonnaci.py
@@ -0,0 +1,12 @@
+Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+Make sure to ask the user to enter the number of numbers in the sequence to generate.
+
+def main():
+	n = input("Enter fibonnaci numbers to generate: ")
+	a = [1,1]
+	for i in range(n-2):
+		a.append(fibonacci(a))
+	print a
+ 
+def fibonacci(num):
+	return (num[-1] + num [-2])
