caa07a5b3125dd09197a44fdc328ccc727bbb418
jineshshinde <samratjinesh619@gmail.com>
2016-06-22 18:35:18 +0530

    Update List_ends.py

--- List_ends.py
+++ List_ends.py
@@ -2,9 +2,8 @@ Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 2
 last elements of the given list.
 
 import random
+def slice(arr):
+    return arr[:1] + arr[-1:] if len(arr) > 1 else arr
 
 a = [random.randint(1,100) for x in range(10)]
 slice(a)
-
-def slice(arr):
-    return arr[:1] + arr[-1:] if len(arr) > 1 else arr
